c1fed9462582e4c2b6a8cba4a7d68e8bab6ec7d9
Code <code@4e7ed759d8f8>
2022-10-18 17:18:54 +0800

    first commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ ./compile.sh docker BOARD=king3399 BRANCH=current RELEASE=bullseye BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=no KERNEL_CONFIGURE=no COMPRESS_OUTPUTIMAGE=sha,gz BOOT_LOGO=no
