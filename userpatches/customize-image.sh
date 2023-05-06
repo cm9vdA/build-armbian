7e27bcbde6f8e60d8d0027e5718c1ccfe98bbbf5
Code <code@OCS>
2023-05-06 21:04:53 +0800

    fix mod preload

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -19,8 +19,8 @@ sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 sed -i 's|security.debian.org|mirrors.ustc.edu.cn/debian-security|g' /etc/apt/sources.list
 sed -i 's|apt.armbian.com|mirrors.ustc.edu.cn/armbian|g' /etc/apt/sources.list.d/armbian.list
 
-# modprobe.d
-echo i2c_dev > /etc/modprobe.d/i2c_dev.conf
+# modules-load.d
+echo i2c_dev > /etc/modules-load.d/i2c_dev.conf
 
 # ZCube1 Max no have WiFi
 if [ "${BOARD}" = "zcube1-max" ]; then
